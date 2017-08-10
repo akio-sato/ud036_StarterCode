33fe7ea4b600a2c897a3487a489140f06a748bd7
Akio Sato <30716032+akio-sato@users.noreply.github.com>
2017-08-10 07:25:40 -0400

    Modified media.py

--- media.py
+++ media.py
@@ -10,6 +10,13 @@ class Movie():
                  movie_title,
                  box_art,
                  trailer_link):
+        """Constructor for Movie object.
+
+        Keyword arguments:
+        movie_title -- the title of the movie
+        box_art -- poster image file URL
+        trailer_link -- YouTube URL
+        """
         self.title = movie_title
         self.poster_image_url = box_art
         self.trailer_youtube_url = trailer_link
